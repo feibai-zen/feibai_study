d12dbc851ea06438cd79fdbdb5425b1ac686f888
cloudpc2c04XPn91@cloudpc2c04.huawei.com <cloudpc2c04XPn91@cloudpc2c04.huawei.com>
2019-07-24 23:43:41 +0800

    modify

--- 12-spring-boot-springmvc-mybatis/src/main/java/com/bjsxt/service/UsersService.java
+++ 12-spring-boot-springmvc-mybatis/src/main/java/com/bjsxt/service/UsersService.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.bjsxt.pojo.Users;
 
 public interface UsersService {
-	
+
 	void addUser(Users users);
+
 	List<Users> findUserAll();
+
 	Users findUserById(Integer id);
+
 	void updateUser(Users users);
+
 	void deleteUserById(Integer id);
-} 
+}

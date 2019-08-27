eaee51757ffc3a7cad0c1f198152abc17c2cfbcc
cloudpc2c04XPn91@cloudpc2c04.huawei.com <cloudpc2c04XPn91@cloudpc2c04.huawei.com>
2019-08-27 17:55:47 +0800

    modify

--- 01-spring-boot-hello/src/main/java/com/bjsxt/controller/HelloWorld.java
+++ 01-spring-boot-hello/src/main/java/com/bjsxt/controller/HelloWorld.java
@@ -3,8 +3,13 @@ package com.bjsxt.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -25,4 +30,34 @@ public class HelloWorld {
 		map.put("msg", "HelloWorld");
 		return map;
 	}
+
+	@RequestMapping(value = "/setCookies", method = RequestMethod.GET)
+	@ResponseBody
+	public String setCookies(HttpServletResponse response) {
+        //HttpServerletRequest 装请求信息类
+        //HttpServerletRespionse 装相应信息的类
+		Cookie cookie = new Cookie("sessionId", "CookieTestInfo");
+		response.addCookie(cookie);
+		return "设置cookie成功！";
+	}
+
+	// 非注解方式获取cookie中对应的key值
+	@RequestMapping(value = "/getCookies", method = RequestMethod.GET)
+	@ResponseBody
+	public String getCookies(HttpServletRequest request) {
+		// HttpServletRequest 装请求信息类
+		// HttpServletRespionse 装相应信息的类
+		// Cookie cookie=new Cookie("sessionId","CookieTestInfo");
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("sessionId")) {
+					return cookie.getValue();
+				}
+			}
+		}
+
+		return null;
+	}
+
 }

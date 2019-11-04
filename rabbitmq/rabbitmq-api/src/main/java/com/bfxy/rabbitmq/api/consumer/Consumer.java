985f4a910bb2bc13b9f522cba2c1a40467df11af
leeyuanlong <xmly@xmlydeMacBook-Pro-2.local>
2019-11-04 13:12:08 +0800

    add rabbitmq spring

---
+++ rabbitmq/rabbitmq-api/src/main/java/com/bfxy/rabbitmq/api/consumer/Consumer.java
@@ -0,0 +1,36 @@
+package com.bfxy.rabbitmq.api.consumer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+
+public class Consumer {
+
+	
+	public static void main(String[] args) throws Exception {
+		
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost("192.168.11.76");
+		connectionFactory.setPort(5672);
+		connectionFactory.setVirtualHost("/");
+		
+		Connection connection = connectionFactory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		
+		String exchangeName = "test_consumer_exchange";
+		String routingKey = "consumer.#";
+		String queueName = "test_consumer_queue";
+		
+		channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+		channel.queueDeclare(queueName, true, false, false, null);
+		channel.queueBind(queueName, exchangeName, routingKey);
+		
+		channel.basicConsume(queueName, true, new MyConsumer(channel));
+		
+		
+	}
+}

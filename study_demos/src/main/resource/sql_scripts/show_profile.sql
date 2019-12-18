ac1162d69ad2d51ec76a4bb4891ea42822bec2ec
Feibai <yuanlong.li@ximalaya.com>
2019-12-18 19:25:39 +0800

    show process list

--- study_demos/src/main/resource/sql_scripts/show_profile.sql
+++ study_demos/src/main/resource/sql_scripts/show_profile.sql
@@ -11,3 +11,15 @@ FROM INFORAMTION_SCHEMA.PROFILING
 WHERE QUERY_ID = @query_id
 GROUP BY STATE
 ORDER BY Total_R DESC;
+
+
+#show status
+#说明: 1:SHOW STATUS显式的是计数器   2:注意查看的会话级别,默认是SESSION,可以设置为GLOBAL   3:通过计算器是实际的测量结果,EXPLAIN是通过估计得到的结果,EXPALIN无法告知临时表是否是磁盘表
+FLUSH STATUS;
+#SELECT * FROM tb_name; #just a example.
+SHOW STATUS WHERE Variable_name LIKE 'Handler%' OR Variable_name LIKE 'Created%';
+
+
+#每秒查询数 | Threads_Connected | Thread_running(正在执行的查询),这三个数据的趋势对于服务器级别偶尔停顿的敏感性很高.该命令每秒钟捕获一次show GLOBAL status的数据
+mysqladmin ext -i1 | awk '/Queries/{q=$4-qp;qp=$4}/Threads_connected/{tc=$4}/Threads_running/{printf "%5d %5d %5d\n",q,tc,$4}'
+

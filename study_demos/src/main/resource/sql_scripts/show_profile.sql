e1130bf72420781aaae0155814c78766f5e14e17
Feibai <yuanlong.li@ximalaya.com>
2019-12-19 13:09:57 +0800

    modify

--- study_demos/src/main/resource/sql_scripts/show_profile.sql
+++ study_demos/src/main/resource/sql_scripts/show_profile.sql
@@ -13,13 +13,20 @@ GROUP BY STATE
 ORDER BY Total_R DESC;
 
 
-#show status
+#show status Page:85
 #说明: 1:SHOW STATUS显式的是计数器   2:注意查看的会话级别,默认是SESSION,可以设置为GLOBAL   3:通过计算器是实际的测量结果,EXPLAIN是通过估计得到的结果,EXPALIN无法告知临时表是否是磁盘表
 FLUSH STATUS;
 #SELECT * FROM tb_name; #just a example.
 SHOW STATUS WHERE Variable_name LIKE 'Handler%' OR Variable_name LIKE 'Created%';
 
 
-#每秒查询数 | Threads_Connected | Thread_running(正在执行的查询),这三个数据的趋势对于服务器级别偶尔停顿的敏感性很高.该命令每秒钟捕获一次show GLOBAL status的数据
+#每秒查询数 | Threads_Connected | Thread_running(正在执行的查询),这三个数据的趋势对于服务器级别偶尔停顿的敏感性很高.该命令每秒钟捕获一次show GLOBAL status的数据 Page:90
+关于这三个指标的说明:Thead_running正常情况下不会超过10,这也与CPU的核心数有密切关系,可以任务连续5秒Thread_running保持20以上为异常情况.超过15可以认为有少量查询堆积.
+
 mysqladmin ext -i1 | awk '/Queries/{q=$4-qp;qp=$4}/Threads_connected/{tc=$4}/Threads_running/{printf "%5d %5d %5d\n",q,tc,$4}'
 
+#查看线程状态,可以查看是否有大量线程出于线程不正常的状态 Page:91
+mysql -e 'SHOW PROCESSLIST\G' |grep State: |sort |uniq -c |sort -rn
+
+#统计慢查询日志中每秒的查询数量 Page:92
+awk '/^# Time:/{print $3, $4, c;c=0}/^# User/{c++}' slow-query.log

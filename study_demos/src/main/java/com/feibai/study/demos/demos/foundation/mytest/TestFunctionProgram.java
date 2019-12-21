a5c7ce60b000841dfdf01fc74d08636e6b2ba3b6
Feibai <yuanlong.li@ximalaya.com>
2019-12-21 13:58:26 +0800

    functionPragram

---
+++ study_demos/src/main/java/com/feibai/study/demos/demos/foundation/mytest/TestFunctionProgram.java
@@ -0,0 +1,31 @@
+package com.feibai.study.demos.demos.foundation.mytest;
+
+/**
+ * @Author feibai
+ **/
+
+public class TestFunctionProgram {
+
+  static {
+    System.out.println("static block.");
+    tryImplementation(TestFunctionProgram::testFunc);
+  }
+
+  public static synchronized void testFunc() {
+    System.out.println("testFunc");
+    setImplementation();
+  }
+
+  private static void setImplementation() {
+    System.out.println("setImplementation");
+  }
+
+  private static void tryImplementation(Runnable runnable) {
+    System.out.println("tryImplementation");
+    runnable.run();
+  }
+
+  public static void main(String[] args) {
+    System.out.println("main");
+  }
+}

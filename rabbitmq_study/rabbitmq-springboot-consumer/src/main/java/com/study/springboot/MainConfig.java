36656918eecba60bfff39332bdca07ce238c4c2f
Feibai <yuanlong.li@ximalaya.com>
2020-03-04 13:28:31 +0800

    modify

--- rabbitmq_study/rabbitmq-springboot-consumer/src/main/java/com/study/springboot/MainConfig.java
+++ rabbitmq_study/rabbitmq-springboot-consumer/src/main/java/com/study/springboot/MainConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"com.bfxy.springboot.*"})
+@ComponentScan({"com.study.springboot.*"})
 public class MainConfig {
 
 }
--- rabbitmq_study/rabbitmq-springboot-producer/src/main/java/com/study/springboot/producer/RabbitSender.java
+++ rabbitmq_study/rabbitmq-springboot-producer/src/main/java/com/study/springboot/producer/RabbitSender.java
@@ -16,50 +16,51 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitSender {
 
-	//自动注入RabbitTemplate模板类
-	@Autowired
-	private RabbitTemplate rabbitTemplate;  
-	
-	//回调函数: confirm确认
-	final ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
-		@Override
-		public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-			System.err.println("correlationData: " + correlationData);
-			System.err.println("ack: " + ack);
-			if(!ack){
-				System.err.println("异常处理....");
-			}
-		}
-	};
-	
-	//回调函数: return返回
-	final ReturnCallback returnCallback = new RabbitTemplate.ReturnCallback() {
-		@Override
-		public void returnedMessage(org.springframework.amqp.core.Message message, int replyCode, String replyText,
-				String exchange, String routingKey) {
-			System.err.println("return exchange: " + exchange + ", routingKey: " 
-				+ routingKey + ", replyCode: " + replyCode + ", replyText: " + replyText);
-		}
-	};
-	
-	//发送消息方法调用: 构建Message消息
-	public void send(Object message, Map<String, Object> properties) throws Exception {
-		MessageHeaders mhs = new MessageHeaders(properties);
-		Message msg = MessageBuilder.createMessage(message, mhs);
-		rabbitTemplate.setConfirmCallback(confirmCallback);
-		rabbitTemplate.setReturnCallback(returnCallback);
-		//id + 时间戳 全局唯一 
-		CorrelationData correlationData = new CorrelationData("1234567890");
-		rabbitTemplate.convertAndSend("exchange-1", "springboot.abc", msg, correlationData);
-	}
-	
-	//发送消息方法调用: 构建自定义对象消息
-	public void sendOrder(Order order) throws Exception {
-		rabbitTemplate.setConfirmCallback(confirmCallback);
-		rabbitTemplate.setReturnCallback(returnCallback);
-		//id + 时间戳 全局唯一 
-		CorrelationData correlationData = new CorrelationData("0987654321");
-		rabbitTemplate.convertAndSend("exchange-2", "springboot.def", order, correlationData);
-	}
-	
+  //自动注入RabbitTemplate模板类
+  @Autowired
+  private RabbitTemplate rabbitTemplate;
+
+  //回调函数: confirm确认
+  final ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+			// correlationData:消息的ID,需要全局唯一
+      System.err.println("correlationData: " + correlationData);
+      System.err.println("ack: " + ack);
+      if (!ack) {
+        System.err.println("异常处理....");//可以做一些补偿操作
+      }
+    }
+  };
+
+  //回调函数: return返回
+  final ReturnCallback returnCallback = new RabbitTemplate.ReturnCallback() {
+    @Override
+    public void returnedMessage(org.springframework.amqp.core.Message message, int replyCode, String replyText,
+                                String exchange, String routingKey) {
+      System.err.println("return exchange: " + exchange + ", routingKey: "
+              + routingKey + ", replyCode: " + replyCode + ", replyText: " + replyText);
+    }
+  };
+
+  //发送消息方法调用: 构建Message消息
+  public void send(Object message, Map<String, Object> properties) throws Exception {
+    MessageHeaders mhs = new MessageHeaders(properties);
+    Message msg = MessageBuilder.createMessage(message, mhs);
+    rabbitTemplate.setConfirmCallback(confirmCallback);
+    rabbitTemplate.setReturnCallback(returnCallback);
+    //id + 时间戳 全局唯一
+    CorrelationData correlationData = new CorrelationData("1234567890");//消息的ID，需要全局唯一
+    rabbitTemplate.convertAndSend("exchange-1", "springboot.abc", msg, correlationData);
+  }
+
+  //发送消息方法调用: 构建自定义对象消息
+  public void sendOrder(Order order) throws Exception {
+    rabbitTemplate.setConfirmCallback(confirmCallback);
+    rabbitTemplate.setReturnCallback(returnCallback);
+    //id + 时间戳 全局唯一
+    CorrelationData correlationData = new CorrelationData("0987654321");
+    rabbitTemplate.convertAndSend("exchange-2", "springboot.def", order, correlationData);
+  }
+
 }

ec55122b69d5f4e266465da22dc94754f1b935be
Feibai <yuanlong.li@ximalaya.com>
2020-03-10 19:28:46 +0800

    modify

--- spring_study/02-spring-boot-servlet/src/main/java/com/feibai/spring/study/App.java
+++ spring_study/02-spring-boot-servlet/src/main/java/com/feibai/spring/study/App.java
@@ -5,16 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
- * SpringBoot整合Servlet方式一
- *
- *
+ * demo演示了三种servlet整合方式：
+ * 1）web.xml的方式
+ * 2）@WebServlet方式
+ * 3）@SpringBootApplication
+ */
+
+/**
+ * SpringBoot整合Servlet方式二
  */
 @SpringBootApplication
 @ServletComponentScan //在springBoot启动时会扫描@WebServlet，并将该类实例化
 public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 
 }
--- spring_study/02-spring-boot-servlet/src/main/java/com/feibai/spring/study/App2.java
+++ spring_study/02-spring-boot-servlet/src/main/java/com/feibai/spring/study/App2.java
@@ -7,21 +7,26 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 /**
- * SpringBoot整合Servlet方式二
- *
- *
+ * demo演示了三种servlet整合方式：
+ * 1）web.xml的方式
+ * 2）@WebServlet方式
+ * 3）@SpringBootApplication
+ */
+
+/**
+ * SpringBoot整合Servlet方式三
  */
 @SpringBootApplication
 public class App2 {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App2.class, args);
-	}
-	
-	@Bean // 通过方法完成Servlet组件的注册
-	public ServletRegistrationBean getServletRegistrationBean() {// 方法名称无所谓
-		ServletRegistrationBean bean = new ServletRegistrationBean(new SecondServlet());
-		bean.addUrlMappings("/second");
-		return bean;
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(App2.class, args);
+  }
+
+  @Bean // 通过方法完成Servlet组件的注册
+  public ServletRegistrationBean getServletRegistrationBean() {// 方法名称无所谓
+    ServletRegistrationBean bean = new ServletRegistrationBean(new SecondServlet());
+    bean.addUrlMappings("/second");
+    return bean;
+  }
 }

d895d6ff063826e015ad7359226a1467b5579129
Feibai <yuanlong.li@ximalaya.com>
2020-03-19 08:22:06 +0800

    modify

--- study_demos/src/main/java/com/feibai/study/demos/demos/multithread/foundation/TerminatedTread.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/multithread/foundation/TerminatedTread.java
@@ -1,43 +1,41 @@
 package com.feibai.study.demos.demos.multithread.foundation;
 
 /**
- * 
  * 通过设置标志位终止线程
- * 
- * @author feibai
  *
+ * @author feibai
  */
 public class TerminatedTread implements Runnable {
 
-	private boolean flag = true;
+  private boolean flag = true;
 
-	public static void main(String[] args) {
-		TerminatedTread thread = new TerminatedTread();
-		new Thread(thread).start();
-		try {
-			Thread.sleep(5000);
-			thread.terminateThread();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+  public static void main(String[] args) {
+    TerminatedTread thread = new TerminatedTread();
+    new Thread(thread).start();
+    try {
+      Thread.sleep(5000);
+      thread.terminateThread();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
-	}
+  }
 
-	@Override
-	public void run() {
-		while (flag) {
-			try {
-				Thread.sleep(200);
-				System.out.println("Thread is running...");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("Thread is terminated...");
-	}
+  @Override
+  public void run() {
+    while (flag) {
+      try {
+        Thread.sleep(200);
+        System.out.println("Thread is running...");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.println("Thread is terminated...");
+  }
 
-	public void terminateThread() {
-		flag = false;
-	}
+  public void terminateThread() {
+    flag = false;
+  }
 
 }
--- study_demos/src/main/java/com/feibai/study/demos/demos/multithread/foundation/join/BlockedJoinTest.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/multithread/foundation/join/BlockedJoinTest.java
@@ -2,33 +2,32 @@ package com.feibai.study.demos.demos.multithread.foundation.join;
 
 /**
  * join:合并线程、线程插队
- * 
- * @author leeyuanlong
  *
+ * @author feibai
  */
 public class BlockedJoinTest {
-	public static void main(String[] args) throws InterruptedException {
-		Thread thread = new Thread(() -> {
-			for (int i = 0; i < 100; i++) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("当前线程： " + Thread.currentThread().getName() + i);
-			}
-		});
-		thread.start();
+  public static void main(String[] args) throws InterruptedException {
+    Thread thread = new Thread(() -> {
+      for (int i = 0; i < 100; i++) {
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        System.out.println("当前线程： " + Thread.currentThread().getName() + i);
+      }
+    });
+    thread.start();
 
-		for (int i = 0; i < 100; i++) {
-			System.out.println("当前线程： " + Thread.currentThread().getName() + i);
+    for (int i = 0; i < 100; i++) {
+      System.out.println("当前线程： " + Thread.currentThread().getName() + i);
 
-			if (i == 20) {
-				System.out.println("当前线程： " + Thread.currentThread().getName() + i + "Blocked");
-				thread.join();// 等待
-			}
+      if (i == 20) {
+        System.out.println("当前线程： " + Thread.currentThread().getName() + i + "Blocked");
+        thread.join();// 等待
+      }
 
-		}
-	}
+    }
+  }
 }

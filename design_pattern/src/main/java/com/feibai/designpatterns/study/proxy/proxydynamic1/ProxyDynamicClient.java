4975f318babad0b5b63b89abb7c211e0a67d1f14
Feibai <yuanlong.li@ximalaya.com>
2020-03-23 18:56:01 +0800

    modify

--- design_pattern/src/main/java/com/feibai/designpatterns/study/proxy/proxydynamic1/ProxyDynamicClient.java
+++ design_pattern/src/main/java/com/feibai/designpatterns/study/proxy/proxydynamic1/ProxyDynamicClient.java
@@ -2,6 +2,10 @@ package com.feibai.designpatterns.study.proxy.proxydynamic1;
 
 import java.lang.reflect.Proxy;
 
+/**
+ * 动态代理与静态代理相比较，最大的好处是接口中声明的所有方法都被转移到调用处理器一个集中的方法中处理（InvocationHandler.invoke）。这样，在
+ * 接口方法数量比较多的时候，我们可以进行灵活处理，而不需要像静态代理那样每一个方法进行中转。而且动态代理的应用使我们的类职责更加单一，复用性更强
+ */
 public class ProxyDynamicClient {
   public static void main(String[] args) {
 

6223ee5a4de810aad7971fa324ab5c4fe46afb81
Leeyuanlong <959769993@qq.com>
2020-04-01 22:59:06 +0800

    modify

---
+++ leetcode/src/main/java/com/feibai/leetcode/study/solution/Lc_05_LongestPalindromicSubstring.java
@@ -0,0 +1,28 @@
+package com.feibai.leetcode.study.solution;
+
+/**
+ * 最长回文子串
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ * <p>
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
+public class Lc_05_LongestPalindromicSubstring {
+
+  public static void main(String[] args) {
+    Lc_05_LongestPalindromicSubstring instance = new Lc_05_LongestPalindromicSubstring();
+    System.out.println(instance.longestPalindrome(""));
+  }
+
+  public String longestPalindrome(String s) {
+
+    return null;
+  }
+}

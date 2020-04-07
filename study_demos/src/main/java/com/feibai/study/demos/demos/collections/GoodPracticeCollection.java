2f8d6adeadc42be219299ed068c732a6253789a1
Feibai <yuanlong.li@ximalaya.com>
2020-04-07 19:12:27 +0800

    modify

--- study_demos/src/main/java/com/feibai/study/demos/demos/collections/GoodPracticeCollection.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/collections/GoodPracticeCollection.java
@@ -1,9 +1,10 @@
-package com.feibai.study.demos.good_practice.collections;
+package com.feibai.study.demos.demos.collections;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
@@ -32,6 +33,9 @@ public class GoodPracticeCollection {
     Sets.newCopyOnWriteArraySet();
     Sets.newConcurrentHashSet();
     Sets.newHashSet();
+
+    List<Integer> testList = Collections.EMPTY_LIST;
+    Collections.emptyList();
   }
 
 }
---
+++ study_demos/src/main/java/com/feibai/study/demos/demos/collections/datastructure/LinkedListTest.java
@@ -0,0 +1,29 @@
+package com.feibai.study.demos.demos.collections.datastructure;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 底层使用双向链表实现，非连续有空间，空间利用率高
+ * <p>
+ * 双向链表：查询快，可以从头节点开始找，也可以从尾节点开始找
+ * <p>
+ * 查询慢，增删快。非线程安全、有序、可以重复、可为null
+ */
+public class LinkedListTest {
+
+  public static void main(String[] args) {
+
+  }
+
+  public void test_01() {
+
+    List<Integer> linkedList = new LinkedList<>();
+
+  }
+
+  public void test_02() {
+
+  }
+}

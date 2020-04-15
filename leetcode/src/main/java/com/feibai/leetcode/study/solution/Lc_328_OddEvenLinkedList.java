35f5b043ab0b1834db4cc29d8a932ebae28ff04d
Feibai <yuanlong.li@ximalaya.com>
2020-04-15 19:30:23 +0800

    doing

--- leetcode/src/main/java/com/feibai/leetcode/study/solution/Lc_328_OddEvenLinkedList.java
+++ leetcode/src/main/java/com/feibai/leetcode/study/solution/Lc_328_OddEvenLinkedList.java
@@ -3,6 +3,8 @@ package com.feibai.leetcode.study.solution;
 
 import com.feibai.leetcode.study.beans.ListNode;
 
+import java.util.List;
+
 /**
  * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
  * <p>
@@ -27,9 +29,55 @@ import com.feibai.leetcode.study.beans.ListNode;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Lc_328_OddEvenLinkedList {
-  public ListNode oddEvenList(ListNode head) {
+  public static void main(String[] args) {
+    Lc_328_OddEvenLinkedList instance = new Lc_328_OddEvenLinkedList();
+    ListNode result = instance.oddEvenList(instance.createList(20));
+    instance.printList(result);
+  }
 
-    return null;
+  public ListNode oddEvenList(ListNode head) {
+    if (null == head) {
+      return head;
+    }
+
+    ListNode odd, even, tmp, tmp_odd, tmp_even;
+    int i = 1;
+
+    tmp = head;
+    while (tmp != null) {
+      if (i % 2 == 0) {
+
+      } else {
+
+      }
+      tmp = tmp.next;
+      i++;
+    }
+    odd.next = even;
+
+    return odd;
+  }
+
+  private ListNode createList(int cnt) {
+    ListNode dummy = new ListNode(0);
+
+    ListNode tmp = dummy;
+    for (int i = 1; i <= cnt; i++) {
+      tmp.next = new ListNode(i);
+      tmp = tmp.next;
+    }
+
+    return dummy.next;
+
+  }
+
+  private void printList(ListNode listNode) {
+
+    ListNode tmp = listNode;
+    while (tmp != null) {
+      System.out.println(tmp.val);
+      tmp = tmp.next;
+    }
   }
 
 }

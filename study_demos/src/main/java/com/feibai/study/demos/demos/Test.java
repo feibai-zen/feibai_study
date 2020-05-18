5cba3a0d07264ffa7fe61aff6c641b32f68fad3a
Feibai <yuanlong.li@ximalaya.com>
2020-05-18 17:29:46 +0800

    modify

--- study_demos/src/main/java/com/feibai/study/demos/demos/Test.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/Test.java
@@ -1,16 +1,19 @@
 package com.feibai.study.demos.demos;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
+import com.alibaba.fastjson.JSON;
+
+import java.util.List;
 
 public class Test {
 
   public static void main(String[] args) {
-    BigDecimal innerValue = BigDecimal.valueOf(100.00000);
-    System.out.println(String.valueOf(innerValue.longValueExact()));
-//    System.out.println(Long.parseLong("20.0"));
 
-    System.out.println(BigDecimal.valueOf(20).multiply(new BigDecimal("0.2")).toString());
+    new Test().test_001();
+  }
+
+  public void test_001() {
+    List<Integer> tempModelList = JSON.parseArray("[123,456,789]", Integer.class);
+    System.out.println(tempModelList);
   }
 
 }

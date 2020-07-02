97b2942499bdbf22f480a46a358db438db12ee86
Feibai <yuanlong.li@ximalaya.com>
2020-07-02 15:33:27 +0800

    optimize

--- spring_study/32-spring-boot-config-study/src/main/java/com/feibai/study/model/People.java
+++ spring_study/32-spring-boot-config-study/src/main/java/com/feibai/study/model/People.java
@@ -1,25 +1,16 @@
 package com.feibai.study.model;
 
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 @ConfigurationProperties(prefix = "people")
+@Setter
 public class People {
   private String name;
   private Integer age;
-
-  private List<String> address;
-
   private String phone;
-
-  public String getName() {
-
-    return name;
-
-  }
 }
 
 
--- study_demos/src/main/java/com/feibai/study/demos/demos/foundation/int_stream/IntStream_test.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/foundation/int_stream/IntStream_test.java
@@ -4,18 +4,18 @@ import java.util.Iterator;
 import java.util.stream.IntStream;
 
 /**
+ * @author feibai
  * @ClassName IntStream_test
  * @Description TODO
- * @author feibai
  * @Date 2019/11/25 8:28 下午
  **/
 
 public class IntStream_test {
 
   public static void main(String[] args) {
-    test01(0, 100);
+    test01(0, 100);//0~99
+    test02(0, 100);//0~100
   }
-
 
   private static void test01(int start, int offset) {
     Iterator<Integer> iterator = IntStream.range(start, start + offset).boxed().iterator();
@@ -25,6 +25,11 @@ public class IntStream_test {
     }
   }
 
+  private static void test02(int start, int offset) {
+    Iterator<Integer> iterator = IntStream.rangeClosed(start, start + offset).boxed().iterator();
+
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+    }
+  }
 }
-
-

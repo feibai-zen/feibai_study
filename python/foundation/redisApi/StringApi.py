d71bbe668b438e2629923d225908a81b1442206f
Feibai <yuanlong.li@ximalaya.com>
2020-07-21 10:19:48 +0800

    modify

---
+++ python/foundation/redisApi/StringApi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+from foundation.redisApi.Utils import Util
+
+
+class StringApi:
+
+  def set(self, util, key, value):
+    r = util.getRedisConnection()
+    r.set(key, value)
+    r.get(key)
+
+
+if __name__ == '__main__':
+  util = Util()
+  test = StringApi()
+  test.set(util, "name", "互动娱乐事业部")
--- python/foundation/redisApi/Utils.py
+++ python/foundation/redisApi/Utils.py
@@ -3,17 +3,18 @@
 
 import redis
 
+host = '192.168.3.131'
+port = 19000
+password = 'jredis123456'
+db = 0
+
 
 class Util:
-  host = '192.168.3.131'
-  port = 19000
-  password = 'jredis123456'
-  db = 0
 
-  def __init__(self, name):
-    self.name = name
+  def __init__(self):
+    pass
 
   def getRedisConnection(self):
-    pool = redis.ConnectionPool(self.host, self.port, self.password, self.db)
+    pool = redis.ConnectionPool(host, port, password)
 
     return redis.Redis(connection_pool=pool)

c3c32d16fda9baf4c30b5581190a49f3ce9702c5
Feibai <yuanlong.li@ximalaya.com>
2020-08-04 12:19:27 +0800

    modify

--- design_pattern/src/main/java/com/feibai/designpatterns/study/singleton/SingletonInnerStaticClass.java
+++ design_pattern/src/main/java/com/feibai/designpatterns/study/singleton/SingletonInnerStaticClass.java
@@ -5,6 +5,7 @@ package com.feibai.designpatterns.study.singleton;
  * <p>
  * 1.外部类没有static属性，则不会像饿汉式那样立即加载对象
  * 2.只有调用getInstance()，才会加载静态内部类。加载类时是线程安全的。instance是static final类
+ * 2.只有调用getInstance()，才会加载静态内部类。加载类时是线程安全的。instance是static final类
  * 型，保证了内存中只有一个实例存在，而且只能被赋值一次，从而保证了线程安全性。
  * 3.兼备了高效调用和延迟加载的优势。加载SingletonInnerStaticClass类时，不会初始化静态内部类，因此具有懒加载。
  *

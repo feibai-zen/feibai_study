a01ea71ee14267c4a5ac1713762a1b5b08397d7d
Feibai <yuanlong.li@ximalaya.com>
2020-08-10 09:46:55 +0800

    modify

--- leetcode/src/main/java/com/feibai/leetcode/study/solution/Lc_Fibonacci.java
+++ leetcode/src/main/java/com/feibai/leetcode/study/solution/Lc_Fibonacci.java
@@ -1,7 +1,12 @@
 package com.feibai.leetcode.study.solution;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lc_Fibonacci {
 
+
+  private int constant = 1000000007;
 
   public static void main(String[] args) {
     System.out.println(solution_1(45));
@@ -35,6 +40,30 @@ public class Lc_Fibonacci {
       second = sum;
     }
     return sum;
+  }
+
+
+  /**
+   * 使用map对动态规划方式进行优化
+   */
+  public int fib(int n) {
+    return fib(n, new HashMap());
+  }
+
+  private int fib(int n, Map<Integer, Integer> map) {
+    if (n < 2) {
+      return n;
+    }
+    if (map.containsKey(n)) {
+      return map.get(n);
+    }
+    int first = fib(n - 1, map) % constant;
+    map.put(n - 1, first);
+    int second = fib(n - 2, map) % constant;
+    map.put(n - 2, second);
+    int res = (first + second) % constant;
+    map.put(n, res);
+    return res;
   }
 
 }

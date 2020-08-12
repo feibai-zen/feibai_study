fbd67866c5f6c280cad61944661bcd56c67b4729
Feibai <yuanlong.li@ximalaya.com>
2020-08-12 10:22:06 +0800

    modify

--- spring_study/33-spring-boot-shardingsphere/src/main/java/com/feibai/study/SpringBootShardingJdbcApplication.java
+++ spring_study/33-spring-boot-shardingsphere/src/main/java/com/feibai/study/SpringBootShardingJdbcApplication.java
@@ -4,6 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * https://www.jianshu.com/p/952108f777a3
+ * <p>
+ * 也可以参考以上demo
+ */
+
 @MapperScan("com.feibai.study.mapper")
 @SpringBootApplication
 public class SpringBootShardingJdbcApplication {

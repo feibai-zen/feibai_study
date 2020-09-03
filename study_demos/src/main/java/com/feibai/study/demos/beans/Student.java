b58befe75449783b9e9b1136e10e2d4284d5706a
Feibai <yuanlong.li@ximalaya.com>
2020-09-03 20:20:23 +0800

    optimizer

---
+++ study_demos/src/main/java/com/feibai/study/demos/beans/Student.java
@@ -0,0 +1,21 @@
+package com.feibai.study.demos.beans;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+  private Long id;
+  private String name;
+  private int age;
+  private String address;
+
+
+}
--- study_demos/src/main/java/com/feibai/study/demos/demos/stream/StreamDemos.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/stream/StreamDemos.java
@@ -2,6 +2,7 @@ package com.feibai.study.demos.demos.stream;
 
 import com.feibai.study.demos.beans.Employee;
 import com.feibai.study.demos.beans.Medal;
+import com.feibai.study.demos.beans.Student;
 import com.feibai.study.demos.beans.User;
 import com.google.common.collect.Lists;
 import org.apache.commons.codec.digest.MessageDigestAlgorithms;
@@ -10,10 +11,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
+/**
+ * 包含了大部分的中间和终止操作。
+ * 中间操作（此类型方法返回的都是Stream）:map (mapToInt, flatMap 等)、 filter、 distinct、 sorted、 peek、 limit、 skip、 parallel、 sequential、 unordered
+ * 终止操作：forEach、 forEachOrdered、 toArray、 reduce、 collect、 min、 max、 count、 anyMatch、 allMatch、 noneMatch、 findFirst、 findAny、 iterator
+ */
 public class StreamDemos {
   public static void main(String[] args) {
-//    test_02();
-    test_distinct();
+    test_stream_skip();
+    test_stream_min();
   }
 
   private static void construct_stream() {
@@ -32,7 +39,7 @@ public class StreamDemos {
 
   public static void test_01() {
     List<Medal> medalList = Lists.newArrayList();
-    medalList.stream().filter(tuple -> tuple.getEndAt() > System.currentTimeMillis())
+    medalList.stream().filter(element -> element.getEndAt() > System.currentTimeMillis())
             .map(tuple -> tuple.getTagNo().toString()).distinct().collect(Collectors.toList());
   }
 
@@ -45,7 +52,6 @@ public class StreamDemos {
   }
 
   private static void test_03() {
-
     List<Employee> employees = new ArrayList<>();
     List<Integer> ids = employees.stream().map(Employee::getId).collect(Collectors.toList());
 
@@ -57,9 +63,27 @@ public class StreamDemos {
    * distinct()
    */
   private static void test_distinct() {
-
     System.out.println(Arrays.asList(1, 2, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
             .stream().distinct().collect(Collectors.toList()));
+
+    //简单字符串的去重
+    List<String> list = Arrays.asList("111", "222", "333", "111", "222");
+    list.stream().distinct().forEach(System.out::println);
+
+
+    //引用对象的去重，引用对象要实现hashCode和equal方法，否则去重无效
+    Student s1 = new Student(1L, "肖战", 15, "浙江");
+    Student s2 = new Student(2L, "王一博", 15, "湖北");
+    Student s3 = new Student(3L, "杨紫", 17, "北京");
+    Student s4 = new Student(4L, "李现", 17, "浙江");
+    Student s5 = new Student(1L, "肖战", 15, "浙江");
+    List<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(s2);
+    students.add(s3);
+    students.add(s4);
+    students.add(s5);
+    students.stream().distinct().forEach(System.out::println);
   }
 
   /**
@@ -68,5 +92,138 @@ public class StreamDemos {
   private void test_sorted() {
     List<User> users = new ArrayList();
     List<User> giftTypeList = users.stream().distinct().sorted(Comparator.comparing(User::getId)).collect(Collectors.toList());
+
+    List<String> list = Arrays.asList("333", "222", "111");
+    list.stream().sorted().forEach(System.out::println);
+
+
+    Student s1 = new Student(1L, "肖战", 15, "浙江");
+    Student s2 = new Student(2L, "王一博", 15, "湖北");
+    Student s3 = new Student(3L, "杨紫", 17, "北京");
+    Student s4 = new Student(4L, "李现", 17, "浙江");
+    List<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(s2);
+    students.add(s3);
+    students.add(s4);
+    students.stream()
+            .sorted((stu1, stu2) -> Long.compare(stu2.getId(), stu1.getId()))
+            .sorted((stu1, stu2) -> Integer.compare(stu2.getAge(), stu1.getAge()))
+            .forEach(System.out::println);
+
   }
+
+  private static void test_filter() {
+    Student s1 = new Student(1L, "肖战", 15, "浙江");
+    Student s2 = new Student(2L, "王一博", 15, "湖北");
+    Student s3 = new Student(3L, "杨紫", 17, "北京");
+    Student s4 = new Student(4L, "李现", 17, "浙江");
+    List<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(s2);
+    students.add(s3);
+    students.add(s4);
+    //筛选年龄大于15岁的学生
+//        return students.stream().filter(s -> s.getAge()>15).collect(Collectors.toList());
+    //筛选住在浙江省的学生
+    students.stream().filter(s -> "浙江".equals(s.getAddress())).collect(Collectors.toList()).forEach(System.out::println);
+  }
+
+  /**
+   * map
+   */
+  private static void test_stream_map() {
+    Student s1 = new Student(1L, "肖战", 15, "浙江");
+    Student s2 = new Student(2L, "王一博", 15, "湖北");
+    Student s3 = new Student(3L, "杨紫", 17, "北京");
+    Student s4 = new Student(4L, "李现", 17, "浙江");
+    List<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(s2);
+    students.add(s3);
+    students.add(s4);
+
+    testMap(students);
+  }
+
+  private static void testMap(List<Student> students) {
+    //在地址前面加上部分信息，只获取地址输出
+    List<String> addresses = students.stream().map(s -> "住址:" + s.getAddress()).collect(Collectors.toList());
+    addresses.forEach(a -> System.out.println(a));
+  }
+
+  private static void test_stream_limit() {
+
+    List<String> list = Arrays.asList("333", "222", "111");
+    list.stream().limit(2).forEach(System.out::println);
+  }
+
+  /**
+   * 集合skip，删除前n个元素
+   */
+  private static void test_stream_skip() {
+
+    List<String> list = Arrays.asList("333", "222", "111");
+    list.stream().skip(2).forEach(System.out::println);
+  }
+
+  /**
+   * reduce--聚合
+   */
+  private static void test_stream_reduce() {
+    //集合reduce,将集合中每个元素聚合成一条数据
+    List<String> list = Arrays.asList("欢", "迎", "你");
+    String appendStr = list.stream().reduce("北京", (a, b) -> a + b);
+    System.out.println(appendStr);
+  }
+
+  /**
+   * min--最小值
+   * max--最大值
+   */
+  private static void test_stream_min() {
+    //求集合中元素的最小值
+    Student s1 = new Student(1L, "肖战", 14, "浙江");
+    Student s2 = new Student(2L, "王一博", 15, "湖北");
+    Student s3 = new Student(3L, "杨紫", 17, "北京");
+    Student s4 = new Student(4L, "李现", 17, "浙江");
+    List<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(s2);
+    students.add(s3);
+    students.add(s4);
+    Student minS = students.stream().min((stu1, stu2) -> Integer.compare(stu1.getAge(), stu2.getAge())).get();
+    System.out.println(minS.toString());
+  }
+
+  /**
+   * match--匹配
+   * anyMatch：Stream 中任意一个元素符合传入的 predicate，返回 true
+   * allMatch：Stream 中全部元素符合传入的 predicate，返回 true
+   * noneMatch：Stream 中没有一个元素符合传入的 predicate，返回 true
+   */
+  private static void test_stream_match() {
+    Student s1 = new Student(1L, "肖战", 15, "浙江");
+    Student s2 = new Student(2L, "王一博", 15, "湖北");
+    Student s3 = new Student(3L, "杨紫", 17, "北京");
+    Student s4 = new Student(4L, "李现", 17, "浙江");
+    List<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(s2);
+    students.add(s3);
+    students.add(s4);
+    Boolean anyMatch = students.stream().anyMatch(s -> "湖北".equals(s.getAddress()));
+    if (anyMatch) {
+      System.out.println("有湖北人");
+    }
+    Boolean allMatch = students.stream().allMatch(s -> s.getAge() >= 15);
+    if (allMatch) {
+      System.out.println("所有学生都满15周岁");
+    }
+    Boolean noneMatch = students.stream().noneMatch(s -> "杨洋".equals(s.getName()));
+    if (noneMatch) {
+      System.out.println("没有叫杨洋的同学");
+    }
+  }
+
 }

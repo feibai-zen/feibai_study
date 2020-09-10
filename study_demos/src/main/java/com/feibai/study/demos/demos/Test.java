6b2b65d69614ca0ffed7ec41cf15745f83c8b966
Feibai <yuanlong.li@ximalaya.com>
2020-09-10 19:05:57 +0800

    modify

--- study_demos/src/main/java/com/feibai/study/demos/demos/Test.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/Test.java
@@ -1,22 +1,18 @@
 package com.feibai.study.demos.demos;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import org.springframework.util.CollectionUtils;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Test {
 
-  public static void main(String[] args) {
-//    System.out.println(test_LocalDate());
-//    System.out.println(test_LocalDate2());
-  }
 
   public void test_001() {
     List<Integer> tempModelList = JSON.parseArray("[123,456,789]", Integer.class);
@@ -45,5 +41,51 @@ public class Test {
   private boolean test_list_is_instanceof_collection() {
     List<Integer> test = Collections.singletonList(1);
     return test instanceof Collection;
+  }
+
+  /**
+   * 将uids按照活动规则进行分组存入redis
+   */
+  public static void groupUidsToRedis(List uids) {
+    if (uids.isEmpty()) {
+      return;
+    }
+    Map<String, String> idToGroup = new HashMap(uids.size());
+    int step;
+    int index = 0;
+    int i = 0;
+    String cacer = "racer_catching_";
+    String groupName;
+    while (index < uids.size() && i <= 10) {
+      if (i == 0) {
+        step = 10;
+        groupName = "racer_king";
+      } else {
+        step = 11;
+        groupName = cacer + i;
+      }
+      for (Object uid : uids.subList(index, index + step)) {
+        idToGroup.put(uid.toString(), groupName);
+      }
+      index += step;
+      i++;
+    }
+//    RedisSerializer<String> serializer = stringRedisTemplate.getStringSerializer();
+//    stringRedisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+//      for (String uid : idToGroup.keySet()) {
+//        connection.set(serializer.serialize(uid), serializer.serialize(idToGroup.get(uid)));
+//      }
+//      return null;
+//    });
+  }
+
+  public static void main(String[] args) {
+    List<String> uids = Lists.newArrayList();
+    Iterator<Integer> iterator = IntStream.range(1, 120).boxed().iterator();
+    while (iterator.hasNext()) {
+      uids.add(iterator.next().toString());
+    }
+
+    groupUidsToRedis(uids);
   }
 }

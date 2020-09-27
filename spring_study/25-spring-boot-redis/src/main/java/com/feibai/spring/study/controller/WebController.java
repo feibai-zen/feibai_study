caac994f73a306e0624e70c71b20b074f28d31f5
Feibai <yuanlong.li@ximalaya.com>
2020-09-27 16:11:26 +0800

    optimize

--- spring_study/25-spring-boot-redis/src/main/java/com/feibai/spring/study/controller/WebController.java
+++ spring_study/25-spring-boot-redis/src/main/java/com/feibai/spring/study/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.feibai.spring.study.controller;
 
+import com.feibai.spring.study.model.People;
 import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -28,7 +30,10 @@ public class WebController {
   @Autowired
   private Environment env;
 
-  @Value("#{'${act.data.ids}'.split(',')}")
+  @Resource
+  private People pepple;
+
+  @Value("#{'${data.ids}'.split(',')}")
   private Set<String> actDataIds;
 
   @RequestMapping(value = "index", method = RequestMethod.GET)
@@ -39,13 +44,16 @@ public class WebController {
   @RequestMapping(value = "index2", method = RequestMethod.GET)
   public String index2() {
     return "The Way 2 : " + env.getProperty("test.msg");
-
   }
 
   @RequestMapping(value = "index3", method = RequestMethod.GET)
   public String index3() {
-
     return StringUtils.join(new ArrayList<String>(actDataIds), ',');
+  }
+
+  @RequestMapping(value = "index4", method = RequestMethod.GET)
+  public String index4() {
+    return pepple.getName() + pepple.getPhone() + pepple.getAge();
   }
 
 

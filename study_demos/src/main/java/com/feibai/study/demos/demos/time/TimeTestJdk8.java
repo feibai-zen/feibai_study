8ede010a46f948bbb858b0e489d3a93b5abe480a
Feibai <yuanlong.li@ximalaya.com>
2020-11-12 16:25:21 +0800

    modify

--- study_demos/src/main/java/com/feibai/study/demos/demos/time/TimeTestJdk8.java
+++ study_demos/src/main/java/com/feibai/study/demos/demos/time/TimeTestJdk8.java
@@ -254,15 +254,7 @@ public class TimeTestJdk8 {
    * Date转换为LocalDateTime
    */
   public void date2LocalDateTime(Date date) {
-    Instant instant = date.toInstant();//An instantaneous point on the time-line.(时间线上的一个瞬时点。)
-    ZoneId zoneId = ZoneId.systemDefault();//A time-zone ID, such as {@code Europe/Paris}.(时区)
-    LocalDateTime localDateTime = instant.atZone(zoneId).toLocalDateTime();
-
-    System.out.println(localDateTime.toString());//2018-03-27T14:07:32.668
-    System.out.println(localDateTime.toLocalDate() + " " + localDateTime.toLocalTime());//2018-03-27 14:48:57.453
-
-    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");//This class is immutable and thread-safe.@since 1.8
-    System.out.println(dateTimeFormatter.format(localDateTime));//2018-03-27 14:52:57
+    LocalDateTime startDateTime = Instant.ofEpochMilli(date.getTime()).atZone(ZoneOffset.ofHours(8)).toLocalDateTime();
   }
 
   /**

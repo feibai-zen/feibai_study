9e7ac461c244291b91f6f9f17fc06e40f543321c
Leeyuanlong <959769993@qq.com>
2021-01-11 21:45:26 +0800

    modify

---
+++ study_demos/src/main/java/com/feibai/study/demos/foundation/TestScanner.java
@@ -0,0 +1,22 @@
+package com.feibai.study.demos.foundation;
+
+import java.util.Scanner;
+
+/**
+ * Scnner
+ */
+public class TestScanner {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("输入名字");
+    String name = scanner.nextLine();
+    System.out.println("请输入年龄");
+    String age = scanner.nextLine();
+    System.out.println("请输入地址");
+    String address = scanner.nextLine();
+
+    System.out.println(name + ":" + age + ":" + address);
+  }
+}
---
+++ study_demos/src/main/java/com/feibai/study/demos/foundation/Test_Math_jdk.java
@@ -0,0 +1,14 @@
+package com.feibai.study.demos.foundation;
+
+/**
+ * 测试java.lang.Math类接口
+ */
+public class Test_Math_jdk {
+
+  public static void main(String[] args) {
+    System.out.println(Math.max(1, 2));
+    System.out.println(Math.ceil(3.1));
+    System.out.println(Math.random());//返回[0,1)之间的double类型数值
+    System.out.println(Math.pow(2,10));
+  }
+}

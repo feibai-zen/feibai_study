3be297fa15ea0463a19eefe63314cfae546c7ff5
Feibai <yuanlong.li@ximalaya.com>
2021-03-28 22:05:31 +0800

    modify

---
+++ leetcode/src/main/java/com/feibai/leetcode/study/solution/Lc_20_IsValid.java
@@ -0,0 +1,37 @@
+package com.feibai.leetcode.study.solution;
+
+import java.util.*;
+
+/**
+ * @Author: yuanlong.li
+ * @Date: Created in 9:06 下午 2021/3/28
+ * @Description: 有效的括号
+ * @Company: xmly
+ */
+
+public class Lc_20_IsValid {
+  public static void main(String[] args) {
+    String s = "[{{}]";
+    System.out.println(isValid(s));
+
+  }
+
+  public static boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      Character current = s.charAt(i);
+      if (stack.size() == 0) {
+        //这里需要特别的注意，如果栈中没有元素时候，peek()会抛出异常
+        stack.push(current);
+        continue;
+      }
+      Character peek = stack.peek();
+      if ((peek.equals('{') && current.equals('}')) || (peek.equals('[') && current.equals(']')) || (peek.equals('(') && current.equals(')'))) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    }
+    return !(stack.size() > 0);
+  }
+}
